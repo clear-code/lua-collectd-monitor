2008dccea7a5fb3d2844dc106965ea71805c975d
Takuro Ashie <ashie@clear-code.com>
2020-11-05 11:21:07 +0900

    Add initial implementation of handling MQTT

--- monitor-remote.lua
+++ monitor-remote.lua
@@ -1,33 +1,94 @@
 local inspect = require('inspect')
 local mqtt = require('mqtt')
 
-print("monitor-remote.lua: dump collectd")
-print(inspect(collectd))
+local mqtt_client
+local mqtt_loop = mqtt.get_ioloop()
+local mqtt_handled = false
 
 function config(conf)
    print("monitor-remote.lua: config")
-   print(inspect(conf))
+
+   mqtt_client = mqtt.client {
+      uri = conf.Host,
+      username = conf.User,
+      password = conf.Password,
+      clean = true,
+   }
+   mqtt_client:on {
+      connect = function(reply)
+	 if reply.rc ~= 0 then
+	    print("Failed to connect to broker: ",
+		  reply:reason_string(), reply)
+	 end
+
+	 subscribe_options = {
+	    topic = conf.CommandTopic,
+	 }
+	 assert(mqtt_client:subscribe(subscribe_options))
+
+	 mqtt_handled = true
+      end,
+
+      subscribe = function(reply)
+	 print(inspect(reply))
+	 mqtt_handled = true
+      end,
+
+      unsubscribe = function(reply)
+	 print(inspect(reply))
+	 mqtt_handled = true
+      end,
+
+      message = function(msg)
+	 assert(mqtt_client:acknowledge(msg))
+	 print("received message", msg)
+	 mqtt_handled = true
+      end,
+
+      acknowledge = function()
+	 print("acknowledge")
+	 mqtt_handled = true
+      end,
+
+      error = function()
+	 print("error")
+	 mqtt_handled = true
+      end,
+
+      close = function()
+	 print("close")
+	 mqtt_handled = true
+      end,
+
+      auth = function()
+	 print("auth")
+	 mqtt_handled = true
+      end,
+   }
+
    return 0
 end
 
 function init()
-   print("monitor-remote.lua: init")
+   mqtt_loop:add(mqtt_client)
    return 0
 end
 
 function read()
    print("monitor-remote.lua: read")
-   return 0
-end
-
-function write(metrics)
-   print("monitor-remote.lua: write")
-   print(inspect(metrics))
+   while true do
+      mqtt_loop:iteration()
+      if not mqtt_handled then
+	 break
+      end
+      mqtt_handled = false
+   end
    return 0
 end
 
 function shutdown()
    print("monitor-remote.lua: shutdown")
+   mqtt_loop:remove(mqtt_client)
    return 0
 end
 
@@ -36,4 +97,3 @@ collectd.register_config(config)
 collectd.register_init(init)
 collectd.register_shutdown(shutdown)
 collectd.register_read(read)
-collectd.register_write(write)
