a35b170cccab5f946df10cfb8249ad02c186bfb9
Takuro Ashie <ashie@clear-code.com>
2020-11-16 14:24:21 +0900

    Fix indent

--- monitor-remote.lua
+++ monitor-remote.lua
@@ -6,36 +6,42 @@ local mqtt_config_json
 local mqtt_thread
 local mqtt_thread_pipe
 
-collectd.register_config(function(conf)
-   collectd.log_debug("monitor-remote.lua: config")
+collectd.register_config(
+   function(conf)
+      collectd.log_debug("monitor-remote.lua: config")
 
-   local lunajson = require('lunajson')
-   mqtt_config_json = lunajson.encode(conf)
+      local lunajson = require('lunajson')
+      mqtt_config_json = lunajson.encode(conf)
 
-   local debug_config_json = mqtt_config_json
-   if conf.Password then
-      conf.Password = "********"
-      debug_config_json = lunajson.encode(conf)
+      local debug_config_json = mqtt_config_json
+      if conf.Password then
+         conf.Password = "********"
+         debug_config_json = lunajson.encode(conf)
+      end
+      collectd.log_debug("config: " .. debug_config_json)
+
+      return 0
    end
-   collectd.log_debug("config: " .. debug_config_json)
+)
 
-   return 0
-end)
+collectd.register_init(
+   function()
+      collectd.log_debug("monitor-remote.lua: init")
+      local conf = mqtt_config
+      mqtt_thread, mqtt_thread_pipe =
+         require('cqueues.thread').start(mqtt_thread_func, mqtt_config_json)
+      return 0
+   end
+)
 
-collectd.register_init(function()
-   collectd.log_debug("monitor-remote.lua: init")
-   local conf = mqtt_config
-   mqtt_thread, mqtt_thread_pipe =
-      require('cqueues.thread').start(mqtt_thread_func, mqtt_config_json)
-   return 0
-end)
-
-collectd.register_shutdown(function()
-   collectd.log_debug("monitor-remote.lua: shutdown")
-   mqtt_thread_pipe:write("finish\n")
-   mqtt_thread:join()
-   return 0
-end)
+collectd.register_shutdown(
+   function()
+      collectd.log_debug("monitor-remote.lua: shutdown")
+      mqtt_thread_pipe:write("finish\n")
+      mqtt_thread:join()
+      return 0
+   end
+)
 
 
 --[[
