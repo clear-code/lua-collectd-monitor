8e4baf0087056752a23b65d4942d9006b6e36d67
Takuro Ashie <ashie@clear-code.com>
2020-12-22 00:07:58 +0900

    Add an example script to replace collectd's config file

---
+++ collectd/monitor/replace-config.lua
@@ -0,0 +1,32 @@
+#!/usr/bin/env luajit
+
+local argparse = require('argparse')
+local parser = argparse("replace-config", "Replace the collectd's config and restart the daemon")
+parser:argument("new_config_path", "A path to collectd's config file")
+parser:option("-c --command", "collectd command path", "/usr/sbin/collectd")
+parser:option("-C --config", "collectd config path", "/etc/collectd/collectd.conf")
+parser:option("-P --pid-file", "collectd pid file path", "/run/collectd.pid")
+local args = parser:parse()
+
+function new_config(path)
+   local file = io.open(path)
+   local config = file:read("*a")
+   file:close()
+   return config
+end
+
+local config = new_config(args.new_config_path)
+local options = {
+   CommandPath = args.command,
+   ConfigPath = args.config,
+   PIDPath = args.pid_file,
+}
+
+local Replacer = require('collectd/monitor/config-replacer')
+local replacer = Replacer.new(config, options)
+local replaceable, err = replacer:prepare()
+if not replaceable then
+   print(err)
+   os.exit(1)
+end
+replacer:run()
