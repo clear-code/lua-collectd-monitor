86c44162ed047b2d3f4e58c804003962b64b7ce1
Takuro Ashie <ashie@clear-code.com>
2021-01-05 18:11:43 +0900

    local: Implement loading monitor-config.json

--- collectd/monitor/local.lua
+++ collectd/monitor/local.lua
@@ -1,4 +1,21 @@
-function config(conf)
+local utils = require('collectd/monitor/utils')
+local inspect = require('inspect')
+local monitor_config
+local default_config = {}
+
+function config(collectd_conf)
+   local conf = utils.copy_table(default_config)
+   utils.merge_table(conf, collectd_conf)
+   if conf.MonitorConfigPath then
+      local err_msg
+      monitor_config, err_msg = utils.load_config(conf.MonitorConfigPath)
+      if err_msg then
+         collectd.log_error(err_msg)
+      end
+      utils.merge_table(conf, monitor_config)
+   end
+
+   collectd.log_debug("config: " .. inspect(conf))
 end
 
 function init()
