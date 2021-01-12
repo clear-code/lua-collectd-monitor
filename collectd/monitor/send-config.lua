b2fe280a9f3a04ffa54ace790c3e5da96c6f910e
Takuro Ashie <ashie@clear-code.com>
2021-01-12 16:46:57 +0900

    send-config: Don't use reuse old client
    When unexpected diconnection is happen, we should use new client to
    reset the state, otherwise it causes unstable behavior.

--- collectd/monitor/send-config.lua
+++ collectd/monitor/send-config.lua
@@ -28,84 +28,88 @@ end
 local config = file:read("*a")
 file:close()
 
-local client = mqtt.client {
-   uri = args.host,
-   username = args.user,
-   password = args.password,
-   secure = args.secure,
-   clean = false,
-}
+function new_client()
+   local client = mqtt.client {
+      uri = args.host,
+      username = args.user,
+      password = args.password,
+      secure = args.secure,
+      clean = false,
+   }
 
-function subscribe()
-   if not args.result_topic then
-      return
+   client:on {
+      connect = function(reply)
+         if reply.rc ~= 0 then
+            io.stderr:write("Failed to connect to broker: ",
+                            reply:reason_string(), "\n")
+            return
+         end
+
+         subscribe(client)
+
+         if published then
+            return
+         end
+
+         math.randomseed(os.clock())
+         local message = {
+            task_id = math.random(1, 2^32),
+            timestamp = os.date("!%Y-%m-%dT%TZ"),
+            config = config
+         }
+         local message_json = lunajson.encode(message)
+
+         print("Send config: " .. message_json)
+
+         local publish_options = {
+            topic = args.topic,
+            payload = message_json,
+            qos = tonumber(args.qos),
+            callback = function(packet)
+               print(inspect(packet))
+               if not args.result_topic then
+                  assert(client:disconnect())
+               end
+            end,
+         }
+         assert(client:publish(publish_options))
+         published = true
+      end,
+
+      message = function(packet)
+         print("Received a result: " .. inspect(packet))
+         received = true
+         assert(client:acknowledge(packet))
+         assert(client:disconnect())
+      end,
+
+      error = function(msg)
+         io.stderr:write(msg, "\n")
+         assert(client:disconnect())
+      end,
+
+      close = function(connection)
+         print("MQTT connection closed: ", connection.close_reason)
+      end,
+   }
+
+   function subscribe(client)
+      if not args.result_topic then
+         return
+      end
+
+      local subscribe_options = {
+         topic = args.result_topic,
+         qos = tonumber(args.qos),
+      }
+      assert(client:subscribe(subscribe_options))
    end
 
-   local subscribe_options = {
-      topic = args.result_topic,
-      qos = tonumber(args.qos),
-   }
-   assert(client:subscribe(subscribe_options))
+   return client
 end
 
-client:on {
-   connect = function(reply)
-      if reply.rc ~= 0 then
-         io.stderr:write("Failed to connect to broker: ",
-                         reply:reason_string(), "\n")
-         return
-      end
-
-      subscribe()
-
-      if published then
-         return
-      end
-
-      math.randomseed(os.clock())
-      local message = {
-         task_id = math.random(1, 2^32),
-         timestamp = os.date("!%Y-%m-%dT%TZ"),
-         config = config
-      }
-      local message_json = lunajson.encode(message)
-
-      print("Send config: " .. message_json)
-
-      local publish_options = {
-         topic = args.topic,
-         payload = message_json,
-         qos = tonumber(args.qos),
-         callback = function(packet)
-            print(inspect(packet))
-            if not args.result_topic then
-               assert(client:disconnect())
-            end
-         end,
-      }
-      assert(client:publish(publish_options))
-      published = true
-   end,
-
-   message = function(packet)
-      print("Received a result: " .. inspect(packet))
-      received = true
-      assert(client:acknowledge(packet))
-      assert(client:disconnect())
-   end,
-
-   error = function(msg)
-      io.stderr:write(msg, "\n")
-      assert(client:disconnect())
-   end,
-
-   close = function(connection)
-      print("MQTT connection closed: ", connection.close_reason)
-   end,
-}
-
-mqtt.run_ioloop(client)
+mqtt.run_ioloop(new_client())
 while args.result_topic and not received do
    -- connection may closed by broker when collectd is restarted
-   mqtt.run_ioloop(client)
+   mqtt.run_ioloop(new_client())
 end
